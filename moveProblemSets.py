4e6d94f5b44080c31a4f973e239fac898af6b477
Delbert Yip <delbert124@gmail.com>
2021-12-11 17:45:40 -0800

    Add files via upload
    Fix bug with deleting files. Refactored.

--- moveProblemSets.py
+++ moveProblemSets.py
@@ -1,48 +1,81 @@
 """Move all PDF files with suffix `prob.pdf`, `sol.pdf`, `sum.pdf`, or `ex*.pdf` into one folder above, then delete all remaining files"""
-import os, glob 
+import os, glob, shutil
 from tkinter import filedialog, Tk 
 from typing import List 
 
 def yes_or_no(question: str) -> bool:
 	"""https://gist.github.com/garrettdreyfus/8153571"""
-    reply = str(raw_input(question+' (y/n): ')).lower().strip()
-    if reply[0] == 'y':
-        return True
-    if reply[0] == 'n':
-        return False
-    else:
-        return yes_or_no("Uhhhh... please enter ")
+	reply = str(input(question+' (y/n): ')).lower().strip()
+	if reply[0] == 'y':
+		return True
+	if reply[0] == 'n':
+		return False
+	else:
+		return yes_or_no("Uhhhh... please enter ")
 
 class mover:
-	def __init__(self, folder: str=None):
-		self._suffixes = ['prob.pdf', 'sol.pdf', 'sum.pdf', 'ex*.pdf']
+	def __init__(self, folder: str=None, suffixes: List[str]=None):
 		
-		self._getFolderPath(folder)
-		paths = self._getFilePaths()
-		self._moveFiles(paths)
+		self._getSuffixes(suffixes)		
+		self._getFolderPath(folder)		
+		paths = self._getFilePaths()		
+		self._moveFiles(paths)		
 		self._deleteRemainder()
+	
+	def _getSuffixes(self, suffixes: List[str]):
+		if suffixes is None:
+			self._suffixes = ['prob.pdf', 'sol.pdf', 'sum.pdf', 'ex*.pdf']
+		else:
+			for i, s in enumerate(suffixes):
+				if '.pdf' == s[:-4]: continue 
+				else:
+					suffixes[i] += '.pdf'
+			
+			self._suffixes = suffixes 
 		
 	def _getFolderPath(self, folder: str):
 		"""start a tkinter instance and retrieve the filename with a tkinter gui"""
 		
 		if folder is not None:
 			self._folderPath = folder 
+			print(f"Selected folder: {folder}")
 			return 		
-		
-		try:
-			root = Tk() 
-			root.filename =  filedialog.askdirectory(
-				initialdir = "/", title = "Select directory", 
-			)
-		except KeyboardInterrupt:
-			print("Dialog closed without setting a folder path. Exiting...")
-			exit()
-		
+
+		root = Tk() 
+		root.filename =  filedialog.askdirectory(
+			initialdir = "/", title = "Select directory", 
+		)
+			
+		if root.filename is None:
+			raise ValueError("No directory selected.")
+			
 		print(f"Selected folder: {root.filename}")
 		self._folderPath = root.filename 
 		
+	def _getFilePaths(self) -> List[str]:
+		"""Search through all subdirectories of `self._folderPath` for files matching `self._suffixes`"""
+		pattern = self._folderPath + "/**/*{0}"
+		
+		all_paths = [] 
+		
+		for suffix in self._suffixes:
+			files = glob.glob( pattern.format(suffix), recursive=True ) 
+			if not files: continue 
+			
+			all_paths.extend(files)
+		
+		if not all_paths: 
+			print(f"No files were found with the pattern:\n {pattern}")
+			return [] 
+				
+		return all_paths 
+	
 	def _moveFiles(self, files: List[str]):
 		"""Move files from their current path to `self._folderPath`"""
+		
+		if not files:
+			print("No files to move")
+			return
 		
 		new_file_name = self._folderPath + "/{0}"
 		
@@ -50,11 +83,16 @@ class mover:
 			fname = os.path.basename(f)
 			try: 
 				os.rename(f, new_file_name.format(fname))
-			except Exception as e:
-				print(f"Failed to move file\n {f}\n due to exception:\n{e}.")
+			except WindowsError as e:
+				if e.winerror == 183:
+					print("File already exists in destination. Removing original.")
+					os.remove(f)
+				else:
+					print(f"Failed to move file\n {f}\n due to exception:\n{e}.")
 		
 		print(f"{len(files)} were successfully moved to {new_file_name[:-3]}")
-		
+		return 
+				
 	def _deleteRemainder(self):
 		"""
 		Delete all files in `self._folderPath` that do not have a .pdf extension
@@ -65,30 +103,24 @@ class mover:
 		if not delete_files:
 			return 			
 		
-		for filename in os.scandir(self._folderPath):
-			if ".pdf" == filename[-4:]: continue 
-			print(f"Removing... {filename}")
-			os.remove(filename)
+		with os.scandir(self._folderPath) as files:
+				
+			for f in files:
+				if ".pdf" == f.name[-4:]: continue 
+				print(f"Removing... {f.name}")
+
+				if os.path.isdir(f.path):
+					shutil.rmtree(f.path)
+				else:
+					os.remove(f.path)
 	
-	def _getFilePaths(self) -> List[str]:
-		"""Search through all subdirectories of `self._folderPath` for files matching `self._suffixes`"""
-		pattern = self._folderPath + "/**/*{0}"
-		
-		all_paths = [] 
-		
-		for suffix in self._suffixes:
-			files = glob.glob( pattern.format(suffix), recursive=True ) 
-			print(suffix)
-			if not files: continue 
-			
-			all_paths.extend(files)
-		
-		if not all_paths: 
-			raise ValueError(f"No files were found with the pattern:\n {pattern}")
-		
-		return all_paths 
-			
-m = mover()
+# defaults are for MIT linalg 
+# p6.041: suffixes = ['sol', 'final', 'f09', 's09', '_assn*', '_L*', '_rec*', '_tut*', '_qu*']
+	
+m = mover(
+	folder = r"C:/Users/delbe/Documents/Code_Repositories/MIT 6.041/6-041sc-fall-2013/final-exam",
+	suffixes = ['sol', 'final', 'f09', 's09', '_assn*', '_L*', '_rec*', '_tut*', '_qu*']
+)
 
 			
 	
